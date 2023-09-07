375fafbed637f7411b40155081cfe0d86002bd33
Charm Thiekshana <charm@spaceitlabs.com>
2023-09-07 06:56:48 +0530

    First build

---
+++ src/screens/Recipe/styles.js
@@ -0,0 +1,92 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+const { width: viewportWidth } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1
+  },
+  carouselContainer: {
+    minHeight: 250
+  },
+  carousel: {},
+
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    //width: '100%',
+    //width:400,
+    height: 900
+  },
+  imageContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    width: viewportWidth,
+    height: 750
+  },
+  paginationContainer: {
+    flex: 1,
+    position: 'absolute',
+    alignSelf: 'center',
+    paddingVertical: 8,
+    marginTop: 400
+  },
+  paginationDot: {
+    width: 8,
+    height: 8,
+    borderRadius: 4,
+    marginHorizontal: 0
+  },
+  infoRecipeContainer: {
+    flex: 1,
+    margin: 25,
+    marginTop: -230,
+    //marginBottom:10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  infoContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  infoPhoto: {
+    height: 20,
+    width: 20,
+    marginRight: 0
+  },
+  infoRecipe: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    marginLeft: 5,
+  },
+  category: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    margin: 10,
+    color: '#2cd18a'
+  },
+  infoDescriptionRecipe: {
+    textAlign: 'left',
+    fontSize: 16,
+    marginTop: 30,
+    margin: 15,
+    color: 'white',
+  },
+  infoRecipeName: {
+    fontSize: 28,
+    margin: 10,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center'
+  }
+});
+
+export default styles;
